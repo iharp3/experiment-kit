35d025e55bec7070bfc382abdca0b9c3d25122cf
ana-e-uk <uribe055@umn.edu>
2025-02-09 22:58:15 -0600

    script to move data into tiledb

---
+++ tiledb/ingest_data.py
@@ -0,0 +1,7 @@
+import xarray as xr
+import tiledb
+import tiledb.cf
+
+nc_data_dir = "/data/iharp-customized-storage/storage/514_data"
+
+tiledb_data_dir = "/data/iharp-customized-storage/storage/tdb"
