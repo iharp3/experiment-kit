8abf04109054b6b31727fa96cdb9de830c012445
ana-e-uk <uribe055@umn.edu>
2025-02-10 18:37:48 -0600

    code to ingest data into tiledb

--- tiledb/ingest_data.py
+++ tiledb/ingest_data.py
@@ -1,7 +1,26 @@
-import xarray as xr
+import os
 import tiledb
 import tiledb.cf
 
-nc_data_dir = "/data/iharp-customized-storage/storage/514_data"
+# mkdir -p /data/tiledb_arrays
+# export TILEDB_DEFAULT_STORAGE_PATH="/data/tiledb_arrays"
 
-tiledb_data_dir = "/data/iharp-customized-storage/storage/tdb"
+nc_data_dir = "/data/iharp-customized-storage/storage/514_data"
+tiledb_data_dir = "/data/iharp-customized-storage/storage/experiments_tdb"
+
+config = tiledb.Config({"vfs.file.enable_filelocks": "false"})  # Recommended for local storage
+ctx = tiledb.Ctx(config)
+
+converter = tiledb.cf.NetCDFToTileDBConverter(output_group_path=tiledb_data_dir)
+
+file_list = ["2m_temperature-2014.nc"]
+# for filename in os.listdir(nc_data_dir):
+for filename in file_list:
+    if filename.endswith(".nc"):
+        file_path = os.path.join(nc_data_dir, filename)
+        print(f"Processing file: {file_path}")
+        converter.add_input_file(input_file=file_path)
+
+# Perform the conversion
+converter.convert()
+print("Conversion complete. Data stored in TileDB at:", tiledb_data_dir)
