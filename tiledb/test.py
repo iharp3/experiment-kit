7743708690e0babfcc4ee870fe52b76c696761c3
ana-e-uk <uribe055@umn.edu>
2025-02-12 20:56:43 -0600

    all raw files are the same, save for leap years

--- tiledb/test.py
+++ tiledb/test.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+import numpy as np
+import pandas as pd
 import xarray as xr
 import tiledb
 
@@ -15,37 +17,54 @@ def load_dataset(nc_path, file_name):
 if __name__ == "__main__":
     same = True
     start = True
+    prev_file = ""
     for file in os.listdir(inputs["nc_data_dir"]):
-        if same:
-            if file.endswith(".nc"):
-                if start:
-                    ds_prev = load_dataset(inputs["nc_data_dir"], file)
-                    print(f"\n\n")
-                    print(f"File: {file}")
-                    print(f"\n\n")
-                    print(ds_prev.dims)
-                    print(f"\n\n")
-                    # print(ds.coords)
-                    start = False
-                    continue
-                ds_cur = load_dataset(inputs["nc_data_dir"], file)
+        # if same:
+        if file.endswith(".nc"):
+            if start:
+                ds_prev = load_dataset(inputs["nc_data_dir"], file)
+                prev_file = file
                 print(f"\n\n")
                 print(f"File: {file}")
-                print(f"\n\n")
                 print(ds_prev.dims)
-                print(f"\n\n")
-                # print(ds.coords)
+                # print(f"\n")
+                start = False
+                continue
+            ds_cur = load_dataset(inputs["nc_data_dir"], file)
+            print(f"\n")
+            print(f"File: {file}")
+            print(ds_cur.dims)
+            # print(f"\n")
 
-                # compare datasets:
-                if ds_prev["time"] == ds_cur["time"]:
-                    pass
-                else:
-                    print(f"\n\n\t\t{ds_prev["time"]}")
-                    print(f"\n\n\t\t{ds_cur["time"]}")
-                    same = False
+            # compare latitude values
+            if (ds_prev["latitude"].values == ds_cur["latitude"].values).all():
+                pass
+            else:
+                print(f"\n\t\tValues are not the same\n\n{len((ds_prev["latitude"].values != ds_cur["latitude"].values).nonzero()[0])}")
+                # same = False
 
-        else:
-            sys.exit()
+            # compare longitude values
+            if (ds_prev["longitude"].values == ds_cur["longitude"].values).all():
+                pass
+            else:
+                print(f"\n\t\tValues are not the same\n\n{len((ds_prev["longitude"].values != ds_cur["longitude"].values).nonzero()[0])}")
+                # same = False
+
+            # compare month/day/hour/minute values
+            d1_time = pd.to_datetime(ds_prev["time"].values)
+            d2_time = pd.to_datetime(ds_cur["time"].values)
+            d1_reduced = [(t.month, t.day, t.hour, t.minute) for t in d1_time]
+            d2_reduced = [(t.month, t.day, t.hour, t.minute) for t in d2_time]
+            if np.array_equal(d1_reduced, d2_reduced):
+                print("The month-day-hour-minute values match exactly.")
+            else:
+                print(f"\t\tMismatches found between {file} and {prev_file}.")
+                # same = False
+
+            prev_file = file
+
+        # else:
+        #     sys.exit()
 
 
 
