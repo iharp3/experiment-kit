e614dee6e775404c56d4f283cb6b9e5854b73439
ana-e-uk <uribe055@umn.edu>
2025-02-15 22:39:15 -0600

    use pd.Timestamp obj to get time idx

--- tiledb/test.py
+++ tiledb/test.py
@@ -93,26 +93,95 @@ def xr_temp_agg(ds):
 def tdb_temp_agg(ar):
     pass
 
+def get_last_hour_idx(day: int) -> int:
+    return (24*day)
+
+def get_hour_idx(day: int) -> int:
+    idx = (24 * day) - 24
+    return idx
+
 if __name__ == "__main__":
     # open_tiledb_array()
-    s = pd.Timestamp("2005-01-01 00:00")
-    e = pd.Timestamp("2005-03-12 08:00")
+    s1 = pd.Timestamp("2014-01-01 00:00")
+    s = pd.Timestamp("2015-01-01 00:00")
+    e = pd.Timestamp("2016-04-13 09:00")
 
-    y, m, d, h = get_whole_period_between(s, e)
-    if y:
-        print("YEARS:", [i for i in range(y[0], y[-1] + 1,1)])
-    if m:
-        print("MONTHS:")
-        print(f"year: {pd.Timestamp(m[0]).year}") #, start month: {pd.Timestamp(m[0]).month_name()}, end month: {pd.Timestamp(m[-1]).month_name()}")
-        idx = [i for i in range(int(pd.Timestamp(m[0]).month), int(pd.Timestamp(m[-1]).month) + 1, 1)]
-        for i in idx:
-            print(pd.Timestamp(year=pd.Timestamp(m[0]).year,month=i,day=1).month_name())
-    if d:
-        print("DAYS:")
-        print(f"year: {pd.Timestamp(d[0]).year}, month: {pd.Timestamp(d[0]).month_name()}")
-        idx = [i for i in range(int(pd.Timestamp(d[0]).day), int(pd.Timestamp(d[-1]).day) + 1, 1)]
-        print(idx)
-    if h:
-        print(f"year: {pd.Timestamp(h[0]).year}, month: {pd.Timestamp(h[0]).month_name()}, day: {pd.Timestamp(h[-1]).day}")
-        idx = [i for i in range(int(pd.Timestamp(h[0]).hour), int(pd.Timestamp(h[-1]).hour) + 1, 1)]
-        print(idx)
+    starting_idx = (s -s1).total_seconds()/3600
+    ending_idx = (e - s1).total_seconds()/3600
+
+    print(starting_idx, ending_idx)
+
+
+
+    # got_year = got_month = got_day = got_hour = False
+
+    # y, m, d, h = get_whole_period_between(s, e)
+
+    # if y:
+    #     got_year = True
+    #     print("YEARS:")
+    #     print([i for i in range(y[0], y[-1] + 1,1)])    # list of all complete years
+    #     y1 = int(inputs["year_indices_hours"][str(y[0])])   # get the index of the first hour of the first year
+    #     y2 = int(inputs["year_indices_hours"][str(y[-1] + 1)]) - 1  # get the last hour of the last year
+    #     print(y1, y2) 
+    # if m:
+    #     got_month = True
+    #     print("MONTHS:")
+    #     year = pd.Timestamp(m[0]).year
+    #     if not got_year:
+    #         y1 = y2 = int(year)
+    #     print(f"yr: {year}") # incomplete year
+    #     idx = [pd.Timestamp(year=pd.Timestamp(m[0]).year,month=i,day=1).month_name() for i in range(int(pd.Timestamp(m[0]).month), int(pd.Timestamp(m[-1]).month) + 1, 1)] # name of the months you want
+    #     print(idx)
+    #     if year in inputs["leap_years"]:
+    #         m1 = inputs["leap_year_month_indices"][idx[0]][0]  # index of first hour of the month
+    #         m2 = inputs["leap_year_month_indices"][idx[-1]][1] #index of last hour of the month
+    #     else:
+    #         m1 = inputs["standard_year_month_indices"][idx[0]][0]  # index of first hour of month
+    #         m2 = inputs["standard_year_month_indices"][idx[-1]][1] # index of last hour of month
+    #     print(m1, m2)
+    # if d:
+    #     got_day = True
+    #     print("DAYS:")
+    #     year = pd.Timestamp(d[0]).year
+    #     month = pd.Timestamp(d[0]).month_name()
+    #     if not got_year:
+    #         y1 = y2 = int(year)
+    #     if not got_month:
+    #         m1 = m2 = int(month)
+    #     print(f"yr: {year}, mo: {month}")
+    #     # print(f"\n\td:{d}")
+    #     idx = [i for i in range(int(pd.Timestamp(d[0]).day), int(len(d) +1 ), 1)]  # list of all days
+    #     print(idx)
+    #     d1 = get_hour_idx(idx[0])    # index of hour of first day
+    #     d2 = get_last_hour_idx(idx[-1])   # index of hour of last day
+    #     print(d1, d2)
+    # if h:
+    #     got_hour = True
+    #     print("HOURS:")
+    #     year = pd.Timestamp(h[0]).year
+    #     month = pd.Timestamp(h[0]).month_name()
+    #     day = pd.Timestamp(h[-1]).day
+    #     if not got_year:
+    #         y1 = y2 = int(year)
+    #     if not got_month:
+    #         m1 = m2 = int(month)
+    #     if not got_day:
+    #         d1 = d2 = int(day)
+    #     print(f"yr: {year}, mo: {month}, day: {day}")
+    #     idx = [i for i in range(int(pd.Timestamp(h[0]).hour), int(pd.Timestamp(h[-1]).hour) + 1, 1)]
+    #     print(idx)
+    #     h1 = idx[0]     # first hour
+    #     h2 = idx[-1]    # last hour 
+    #     print(h1, h2)
+    
+    # if not got_hour:
+    #     h1 = 0
+    #     h2 = 0
+
+    # print(f"y1: {y1}, m1: {m1}, d1: {d1}, h1: {h1}")
+    # print(f"y2: {y2}, m2: {m2}, d2: {d2}, h2: {h2}")
+    # start_idx = y1 + m1 + d1 + h1
+    # end_idx = y2 + m2 + d2 + h2
+    # print(f"starting index: {start_idx}")
+    # print(f"ending index: {end_idx}")
--- tiledb/utils.py
+++ tiledb/utils.py
@@ -9,48 +9,19 @@ json_file = "/data/experiment-kit/tiledb/config.json"
 with open(json_file, "r") as f:
     inputs = json.load(f)
 
-def get_day_idx(day: int) -> int:
-       idx = (24 * day) - 24
-       return idx
-
 def get_time_indices(start_time, end_time):
-        y, m, d, h = get_whole_period_between(start=pd.Timestamp(start_time), end=pd.Timestamp(end_time))
-        if y:
-                print("YEARS:", [i for i in range(y[0], y[-1] + 1, 1)])
-        if m:
-                print("MONTHS:")
-                print(f"year: {pd.Timestamp(m[0]).year}")
-                idx = [i for i in range(int(pd.Timestamp(m[0]).month), int(pd.Timestamp(m[-1]).month) + 1, 1)]
-                for i in idx:
-                        print(pd.Timestamp(year=pd.Timestamp(m[0]).year,month=i,day=1).month_name())
-        if d:
-                print("DAYS:")
-                print(f"year: {pd.Timestamp(d[0]).year}, month: {pd.Timestamp(d[0]).month_name()}")
-                idx = [i for i in range(int(pd.Timestamp(d[0]).day), int(pd.Timestamp(d[-1]).day) + 1, 1)]
-                print(idx)
-        if h:
-                print(f"year: {pd.Timestamp(h[0]).year}, month: {pd.Timestamp(h[0]).month_name()}, day: {pd.Timestamp(h[-1]).day}")
-                idx = [i for i in range(int(pd.Timestamp(h[0]).hour), int(pd.Timestamp(h[-1]).hour) + 1, 1)]
-                print(idx)
-        # for each whole year, you have to open the correct array and deal with it, 
-        # the indices are all of them
-        # for the month residuals, you get the year and slice just those months, 
-        # so need the start index of the first month and the end index of the last month
-        # for the day residuals, you need to know the starting index (which should be at the last month) 
-        # get all the month/day/hour indices for leap years and standard years (the hour months start and end, the hour days start)
+        starting_idx = int((start_time - inputs["start_time"]).total_seconds()/3600)
+        ending_idx = int((end_time - inputs["start_time"]).total_seconds()/3600)    # [starting_idx, ending_idx) 
 
-        # define a function that, given the hour a month starts, can give you the starting hour index of a specified day, and the ending hour index of a specified day
-
-        # define a function that, given the starting hour of a day, can give you the starting hour index of a specified hour
-
+        return starting_idx, ending_idx
 
 def get_spatial_range(max_lat, min_lat, max_lon, min_lon):
-        pass
-        # define a function that gives you the start and end indices for a specified spatial region (only need to think about 0.25 res)
+        max_lat_idx = max_lat + inputs["latitude_shift"]
+        min_lat_idx = min_lat + inputs["latitude_shift"]
+        max_lon_idx = max_lon + inputs["longitude_shift"]
+        min_lon_idx = min_lon + inputs["longitude_shift"]
 
-def get_raster(*args):
-        pass
-        # use get_time_range and get_spatial range to return a raster (?) [CHECK what proposed returns: .nc?] 
+        return max_lat_idx, min_lat_idx, max_lon_idx, min_lon_idx
 
 def time_aggregation(res, data):
 
