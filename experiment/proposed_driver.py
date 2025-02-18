f089e01b7f11ba1e6f2de9ca5e887d3bb26befe1
Yuchuan Huang <itisyuchuan@gmail.com>
2025-02-18 11:13:25 -0600

    experiment frame

---
+++ experiment/proposed_driver.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import sys
+import time
+
+# add the path to the sys.path
+sys.path.append("../proposed")
+from query_executor_get_raster import GetRasterExecutor
+
+
+def run_query(q):
+    start_time = time.time()
+    qe = GetRasterExecutor(
+        variable=q["variable"],
+        start_datetime=q["start_time"],
+        end_datetime=q["end_time"],
+        max_lat=q["max_lat"],
+        min_lat=q["min_lat"],
+        min_lon=q["min_lon"],
+        max_lon=q["max_lon"],
+        spatial_resolution=q["spatial_resolution"],
+        temporal_resolution=q["temporal_resolution"],
+        aggregation=q["aggregation"],
+    )
+    try:
+        qe.execute()
+    except Exception as e:
+        print(q)
+        print(e)
+        return -1
+    return time.time() - start_time
+
+
+if __name__ == "__main__":
+    df_query = pd.read_csv("queries/get_raster_test_set.csv")
+
+    time_list = []
+
+    for query in df_query.to_records():
+        print(query)
+        execution_time = run_query(query)
+        print(execution_time)
+        time_list.append(execution_time)
+        print("======================\n")
+
+    df_query["execution_time"] = time_list
+    current_time = time.strftime("%m%d-%H%M%S")
+    df_query.to_csv(f"results/proposed_get_raster_test_result_{current_time}.csv", index=False)
