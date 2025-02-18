470611f9d58ef6c51a1ef4d718613643b808241a
ana-e-uk <uribe055@umn.edu>
2025-02-18 13:20:35 -0600

    driver for tiledbdraft

---
+++ experiment/tiledb_driver.py
@@ -0,0 +1,43 @@
+import multiprocessing
+import pandas as pd
+import sys
+import time
+
+# add the path to the sys.path
+sys.path.append("..")
+from tiledb.tiledb_get_raster_executor import tiledb_get_raster_executor
+
+def run_query(q):
+    start_time = time.time()
+    qe = tiledb_get_raster_executor(
+        variable=q["variable"],
+        start_datetime=q["start_time"],
+        end_datetime=q["end_time"],
+        max_lat=q["max_lat"],
+        min_lat=q["min_lat"],
+        min_lon=q["min_lon"],
+        max_lon=q["max_lon"],
+        spatial_resolution=q["spatial_resolution"],
+        temporal_resolution=q["temporal_resolution"],
+        aggregation=q["aggregation"],
+    )
+    try:
+        qe.execute()
+    except Exception as e:
+        print(q)
+        print(e)
+        return -1
+    return time.time() - start_time
+
+
+def main():
+    df_query = pd.read_csv("queries/get_raster_test_set_tiledb.csv")
+
+    with multiprocessing.Pool(process=multiprocessing.cpu_count()) as pool:
+        time_list = pool.map(run_query, df_query.to_dict(orient="records"))
+    df_query["execution_time"] = time_list
+    current_time = time.strftime("%m%d-%H%M%S")
+    df_query.to_csv(f"results/tiledb_get_raster_test_result_{current_time}.csv", index=False)
+
+if __name__ == "__main__":
+    main()
